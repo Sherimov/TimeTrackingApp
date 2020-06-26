321757bc050d0df0b45aaa68c724a3a05be76297
Sherimov <vasil.sherimov@yahoo.com>
2020-06-26 16:48:50 +0200

    Advanced linq exercise

---
+++ Homework04/SEDC.Adv.AdvLinq/SEDC.Adv.AdvLinqData/Models/Dog.cs
@@ -0,0 +1,28 @@
+﻿using SEDC.Adv.AdvLinqData.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Adv.AdvLinqData.Models
+{
+    public class Dog
+    {
+		public string Name { get; set; }
+		public string Color { get; set; }
+		public int Age { get; set; }
+		public Race Race { get; set; }
+
+		public Dog(string name, string color, int age, Race race)
+		{
+			Name = name;
+			Color = color;
+			Age = age;
+			Race = race;
+		}
+
+		public string PrintInfo()
+		{
+			return $"Dog's name: {Name}, Age: {Age}";
+		}
+	}
+}
---
+++ Homework04/SEDC.Adv.AdvLinq/SEDC.Adv.AdvLinqData/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using SEDC.Adv.AdvLinqData.Enums;
+using SEDC.Adv.AdvLinqData.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SEDC.Adv.AdvLinqData
+{
+    public class Person
+    {
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+		public Job Occupation { get; set; }
+		public List<Dog> Dogs { get; set; }
+
+		public Person(string firstName, string lastName, int age, Job occupation)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Age = age;
+			Occupation = occupation;
+			Dogs = new List<Dog>();
+		}
+
+		public string PrintInfo()
+		{
+			return $"FirstName: {FirstName}, LastName: {LastName}, Age: {Age}";
+		}
+	}
+}
